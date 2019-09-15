0e694c18c4c8781ce4a6017596d041176bb03b64
xDura <3shappyriding@gmail.com>
2019-09-15 23:03:24 +0200

    starting level implementation

---
+++ XEngine/src/level.cc
@@ -0,0 +1,17 @@
+#include "level.h"
+
+void Level::SaveXML(const char * a_path) const
+{
+}
+
+void Level::LoadXML(const char * a_path)
+{
+}
+
+void Level::ReloadXML()
+{
+}
+
+void Level::Delete()
+{
+}
---
+++ XEngine/src/level.h
@@ -0,0 +1,23 @@
+#ifndef LEVEL_H
+#include "ECS/entity_manager.h"
+#include "tilemap.h"
+#include "sprite_sheet.h"
+#include <vector>
+#include "math.h"
+
+class Level
+{
+public:
+	std::vector<Tilemap*> tilemaps;
+	std::vector<int> tilemap_layers;
+	Vector3 position;
+
+	char* path;
+	std::vector<Entity*> level_entities;
+
+	void SaveXML(const char* a_path) const;
+	void LoadXML(const char* a_path);
+	void ReloadXML();
+	void Delete();
+};
+#endif //LEVEL_H
