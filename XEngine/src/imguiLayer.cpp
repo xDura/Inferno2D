f83088413dec514ce93b2fd02a5f048c5e35cd2b
xDura <3shappyriding@gmail.com>
2019-10-18 11:01:29 +0200

    small changes on imgui button size

--- XEngine/src/imguiLayer.cpp
+++ XEngine/src/imguiLayer.cpp
@@ -59,7 +59,7 @@ void ImguiLayer::RenderEditorMenu()
 	{
 		int windowWidth, windowHeight;
 		SDL_GetWindowSize(Game::window, &windowWidth, &windowHeight);
-		bool buttonPressed = ImGui::ImageButton((void*)Game::tileTex->tex_id, ImVec2(windowWidth*editorWindowWidthPercent, windowHeight*editorWindowHeightPercent), ImVec2(0, 0), ImVec2(1, 1), 3, ImVec4(0.0f, 0.0f, 0.0f, 0.0f), ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
+		bool buttonPressed = ImGui::ImageButton((void*)Game::tileTex->tex_id, ImVec2(windowWidth*editorWindowWidthPercent, windowHeight*editorWindowHeightPercent), ImVec2(0, 1), ImVec2(1, 0), 3, ImVec4(0.0f, 0.0f, 0.0f, 0.0f), ImVec4(1.0f, 1.0f, 1.0f, 1.0f));
 		if (buttonPressed)
 		{ 
 			LOG("Button Pressed!"); 
